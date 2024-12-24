a188c787e29faf5226b916c5cecd2f8d031f4fcc
sanha <heftycornerstone@gmail.com>
2024-12-24 15:28:45 +0900

    Feat: context api 추가

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,14 +1,17 @@
-import MealCalendar from '@/components/calendar/MealCalendarSection'
-import React from 'react'
+import CalendarDateContext from '@/components/calendar/CalendarDateContext';
+import MealCalendarSection from '@/components/calendar/MealCalendarSection';
+import React from 'react';
 
 const Calendar = () => {
   return (
     <>
       <h1 className="common-title">ooo님의 식단 달력</h1>
-      <MealCalendar />
-      <div></div>
+      <CalendarDateContext>
+        <MealCalendarSection />
+        <div></div>
+      </CalendarDateContext>
     </>
   );
-}
+};
 
-export default Calendar
+export default Calendar;
