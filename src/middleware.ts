e46a94809bd85a60ec5f7a90d1b127e5b5b96531
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-12-27 19:44:43 +0900

    fix : 미들웨어 updateSession 전체 경로에 통과하도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,16 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { updateSession } from './utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+  //   const publicPaths = ['/signin', '/signup', '/api'];
+  //   const isPublicPath = publicPaths.some((path) => request.nextUrl.pathname.startsWith(path));
+
+  //   if (isPublicPath) {
+  //     return updateSession(request);
+  //   }
+
+  //   return NextResponse.next();
+  // }
   return await updateSession(request);
-  // return await NextResponse.next();
 }
 
 export const config = {
