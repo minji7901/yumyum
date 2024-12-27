68b0694410110e491437f118ca7fc8246ce099ef
ParkGana <gnchoco97@gmail.com>
2024-12-27 15:48:50 +0900

    refactor : 페이지네이션 관련 타입을 제네릭 타입으로 선언해서 사용

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,26 +1,17 @@
 import { FoodType } from '@/types/Food';
+import { PageProps, PaginationType } from '@/types/Pagination';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 interface SearchProps {
   keyword: string;
 }
 
-interface PageProps {
-  pageParam?: number;
-}
-
-interface ResultType {
-  data: FoodType[];
-  nextPage: number;
-  hasMore: boolean;
-}
-
 export const useSearch = ({ keyword }: SearchProps) => {
-  const { data, fetchNextPage, hasNextPage, isPending, isError } = useInfiniteQuery<ResultType, Error>({
+  const { data, fetchNextPage, hasNextPage, isPending, isError } = useInfiniteQuery<PaginationType<FoodType>, Error>({
     queryKey: ['search', keyword],
     queryFn: async ({ pageParam = 1 }: PageProps) => {
       const res = await fetch(`/api/search?page=${pageParam}&keyword=${encodeURIComponent(keyword)}`);
-      const data: ResultType = await res.json();
+      const data: PaginationType<FoodType> = await res.json();
       return data;
     },
     getNextPageParam: (lastPage) => (lastPage.hasMore ? lastPage.nextPage : undefined),
