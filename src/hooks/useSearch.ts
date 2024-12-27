ec84ee5abddb83afb4c741e8814498995e45e9b4
ParkGana <gnchoco97@gmail.com>
2024-12-27 15:50:53 +0900

    refactor : queryFn 내용을 별도의 함수로 분리해서 사용

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -7,13 +7,15 @@ interface SearchProps {
 }
 
 export const useSearch = ({ keyword }: SearchProps) => {
+  const fetchData = async ({ pageParam = 1 }: PageProps) => {
+    const res = await fetch(`/api/search?page=${pageParam}&keyword=${encodeURIComponent(keyword)}`);
+    const data: PaginationType<FoodType> = await res.json();
+    return data;
+  };
+
   const { data, fetchNextPage, hasNextPage, isPending, isError } = useInfiniteQuery<PaginationType<FoodType>, Error>({
     queryKey: ['search', keyword],
-    queryFn: async ({ pageParam = 1 }: PageProps) => {
-      const res = await fetch(`/api/search?page=${pageParam}&keyword=${encodeURIComponent(keyword)}`);
-      const data: PaginationType<FoodType> = await res.json();
-      return data;
-    },
+    queryFn: fetchData,
     getNextPageParam: (lastPage) => (lastPage.hasMore ? lastPage.nextPage : undefined),
     enabled: !!keyword
   });
