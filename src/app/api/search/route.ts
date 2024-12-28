06903dd06fbc167f6b867d5e66ce19ffc5d2ac98
ParkGana <gnchoco97@gmail.com>
2024-12-28 17:29:15 +0900

    feat : 무한 스크롤 기능 잠시 제거

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,12 +4,11 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
 
-  const page = searchParams.get('page');
   const keyword = searchParams.get('keyword');
 
   try {
     const res = await fetch(
-      `https://apis.data.go.kr/1471000/FoodNtrCpntDbInfo01/getFoodNtrCpntDbInq01?serviceKey=${process.env.API_KEY}&type=json&pageNo=${page}&numOfRows=10&FOOD_NM_KR=${keyword}`
+      `https://apis.data.go.kr/1471000/FoodNtrCpntDbInfo01/getFoodNtrCpntDbInq01?serviceKey=${process.env.API_KEY}&type=json&pageNo=1&numOfRows=10&FOOD_NM_KR=${keyword}`
     );
 
     if (!res.ok) {
@@ -19,11 +18,7 @@ export async function GET(request: Request): Promise<NextResponse> {
     const data = await res.json();
     const searchData: SearchType = data.body;
 
-    return NextResponse.json({
-      data: searchData.items,
-      nextPage: searchData.pageNo + 1,
-      hasMore: searchData.pageNo * searchData.numOfRows < searchData.totalCount
-    });
+    return NextResponse.json(searchData);
   } catch (error) {
     console.log('error => ', error);
     return NextResponse.json({ error: 'Internal server error occurred' }, { status: 500 });
