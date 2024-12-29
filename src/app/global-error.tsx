9dfbafbc2b396d752432deacc22d466214cdaf50
minji7901 <kimem7901@gmail.com>
2024-12-29 16:19:04 +0900

    chore : merge

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -20,7 +20,7 @@ const GlobalError = ({ error, reset }: { error: Error & { digest?: string }; res
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
               />
             </svg>
-            <h2 className="text-2xl font-bold text-gray-800 mb-2">문제가 발생했습니다!</h2>
+            <h2 className="text-2xl font-bold text-gray-800 mb-2">문제가 발생했습니다😱</h2>
             <p className="text-gray-600 mb-6">{error.message}</p>
           </div>
           <button
