b43a33e75c3470acc77e66dbc60b46cdc8011acb
dongeul24 <gogo0232@naver.com>
2024-12-29 20:44:06 +0900

    fix: lottie 에러 해결 시도1

--- src/components/todaysmeal/todaysmeal-lottie/ProgressLoadingAnimation.tsx
+++ src/components/todaysmeal/todaysmeal-lottie/ProgressLoadingAnimation.tsx
@@ -1,15 +1,14 @@
-"use client"
+'use client';
 
-import Lottie from 'react-lottie-player';
+import dynamic from 'next/dynamic';
 
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
 import lottieJson from '../../../../public/lottie/Lottie-loading.json';
-
-import React from 'react';
 
 const ProgressLoadingAnimation = () => {
   return (
     <div>
-      <Lottie loop animationData={lottieJson} play/>
+      <Lottie loop animationData={lottieJson} play />
     </div>
   );
 };
--- src/components/todaysmeal/todaysmeal-lottie/ResultAnimation.tsx
+++ src/components/todaysmeal/todaysmeal-lottie/ResultAnimation.tsx
@@ -1,15 +1,14 @@
-"use client"
+'use client';
 
-import Lottie from 'react-lottie-player';
+import dynamic from 'next/dynamic';
 
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
 import lottieJson from '../../../../public/lottie/Lottie-result.json';
-
-import React from 'react';
 
 const ResultAnimation = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <Lottie loop animationData={lottieJson} play className='w-2/3'/>
+    <div className="flex justify-center items-center">
+      <Lottie loop animationData={lottieJson} play className="w-2/3" />
     </div>
   );
 };
